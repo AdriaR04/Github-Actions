6ddc28163e7f00f7723a47a60ad864bcf738f5f8
AdriaR04 <adriarogo1@gmail.com>
2024-03-04 09:53:21 +0100

    ..

---
+++ gitactions/src/App.vue
@@ -0,0 +1,36 @@
+
+
+<template>
+  <div id="app">
+    <div v-for="number in numbers" :key="number" class="key" @click="showNumber(number)">
+      {{ number }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    };
+  },
+  methods: {
+    showNumber(number) {
+      alert(`Has clicat el número: ${number}`);
+    },
+  },
+};
+</script>
+
+<style>
+.key {
+  display: inline-block;
+  padding: 10px;
+  margin: 5px;
+  background: #eee;
+  cursor: pointer;
+}
+</style>
+
